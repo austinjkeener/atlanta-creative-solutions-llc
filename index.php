cf9f0f536855def5cee0728fbdff7afa6138e784
Austin_Keener <austin@atlantacreative.org>
2022-05-24 23:46:53 -0400

    initial file structure setup and homepage skeleton

---
+++ index.php
@@ -0,0 +1,85 @@
+<?php
+if (isset($_POST['submit'])) {
+  if (isset($_POST['g-recaptcha-response']) && !empty($_POST['g-recaptcha-response'])) {
+    $secret = '';
+    $verifyResponse = file_get_contents('https://www.google.com/recaptcha/api/siteverify?secret=' . $secret . '&response=' . $_POST['g-recaptcha-response']);
+    $responseData = json_decode($verifyResponse);
+    if ($responseData->success) {
+      $mailTo = "austin@atlantacreative.org";
+      $txt = "NEW LEAD";
+      $name = $_POST['name'];
+      $email = $_POST['email'];
+      $tel = $_POST['tel'];
+      $message = "~Contact information~" . "\r\n" . "Name: " . $name . "\r\n" . "Email: " . $email . "\r\n" . "Phone: " . $tel . "\r\n" . "Lead Source: Homepage";
+      mail($mailTo, $txt, $message);
+      $headers = "From: Atlanta Creative Solutions LLC <austin@atlantacreative.org>\r\n";
+      header("Location:thanks.php?mailsend");
+    } else {
+      header("Location:index.php");
+    }
+  }
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-2X8MYVB6MQ"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+
+        function gtag() {
+            dataLayer.push(arguments);
+        }
+        gtag('js', new Date());
+
+        gtag('config', 'G-2X8MYVB6MQ');
+    </script>
+
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!--Meta data for social media website sharing-->
+    <meta property="og:url" content="https://www.keenerlaw.com" />
+    <meta property="og:image" content="https://www.keenerlaw.com/images/logos/keenerlaw-post-preview.png" />
+    <meta property="og:title" content="The Keener Law Firm" />
+    <meta property="og:description" content="Welcome to The Keener Law Firm's website." />
+    <meta property="og:image:width" content="891" />
+    <meta property="og:image:height" content="630" />
+
+    <title>Atlanta Creative Solutions LLC</title>
+
+    <!-- Bootstrap CDN-->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous" />
+
+    <!-- Main CSS -->
+    <link href="style.css" rel="stylesheet" media="screen" />
+    <!-- For Chrome for Android: -->
+    <link rel="icon" sizes="192x192" href="/images/">
+    <!-- For iPhone 6 Plus with @3× display: -->
+    <link rel="apple-touch-icon-precomposed" sizes="180x180" href="/images/">
+    <!-- For iPad with @2× display running iOS ≥ 7: -->
+    <link rel="apple-touch-icon-precomposed" sizes="152x152" href="/images/">
+    <!-- For iPad with @2× display running iOS ≤ 6: -->
+    <link rel="apple-touch-icon-precomposed" sizes="144x144" href="/images/">
+    <!-- For iPhone with @2× display running iOS ≥ 7: -->
+    <link rel="apple-touch-icon-precomposed" sizes="120x120" href="/images/">
+    <!-- For iPhone with @2× display running iOS ≤ 6: -->
+    <link rel="apple-touch-icon-precomposed" sizes="114x114" href="/images/">
+    <!-- For the iPad mini and the first- and second-generation iPad (@1× display) on iOS ≥ 7: -->
+    <link rel="apple-touch-icon-precomposed" sizes="76x76" href="/images/">
+    <!-- For the iPad mini and the first- and second-generation iPad (@1× display) on iOS ≤ 6: -->
+    <link rel="apple-touch-icon-precomposed" sizes="72x72" href="/images/">
+    <!-- For non-Retina iPhone, iPod Touch, and Android 2.1+ devices: -->
+    <link rel="apple-touch-icon-precomposed" sizes="57x57" href="/images/"><!-- 57×57px -->
+
+</head>
+
+<body>
+
+</body>
+
+</html>
